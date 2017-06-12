0ec12dee731d701f08ee015741a244d366f4e2f1
meupc <meupc@meupc.timwe.com>
2017-06-12 14:09:27 +0100

    add comment to header to run fab

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+#fab -f test.py 192.168.19.102 listadir
+
 
 from fabric.api import run,env
 
@@ -7,3 +9,6 @@ env.user='root'
 def host_type():
 	#run('uname -s')
 	run('dir')
+
+def listadir():
+    local("dir")
