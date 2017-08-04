95f93f1883ff968138b9ae94bcdf445ddade8f63
mayto <machves@yahoo.com>
2017-08-04 14:38:22 +0100

    add new settings

--- test.py
+++ test.py
@@ -1,15 +1,29 @@
+#! /usr/local/bin/python 
+
 #fab -f test.py -H 192.168.19.102 listadir
 
 
 
 from fabric.api import run,env
+from fabric.operations import local
+from fabric.context_managers import cd
+from fabric.contrib.files import exists
+import logging
 
 #user of system
-env.user='root'
+#env.user='broker'
+env.use_ssh_config = True
 
 def host_type():
 	#run('uname -s')
-	run('dir')
+    with cd('/opt/deployment/fabric/'):
+	if exists("nova_testa"):
+	 #run('mkdir nova_testa')
+         print ("no vale apena")
+	else:
+	  run('mkdir nova_testa')
+	 #logging.warning('no vale apena')	
+	listadir()
 
 def listadir():
-    local("ls")
+    run("ls -lart")
